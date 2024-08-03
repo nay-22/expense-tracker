694891432ebbd6f2581850c0137d71723be727f4
Nayan <nayan92001@gmail.com>
2024-08-04 04:05:49 +0530

    ExpenseTrends Stub => TODO: Implement bar chart summary of expenses and other possible ways of categorizing expenses (weekly-basis, etc.)

---
+++ src/components/ExpenseTrends/ExpenseTrends.jsx
@@ -0,0 +1,14 @@
+import styles from "./ExpenseTrends.module.css";
+
+const ExpenseTrends = () => {
+    return <>
+        <div className={styles.trends}>
+            <h1>Expense Trends</h1>
+            <section className={styles.chart}>
+
+            </section>
+        </div>
+    </>
+}
+
+export default ExpenseTrends
