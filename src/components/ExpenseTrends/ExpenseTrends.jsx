d888c403bd2dca5d0ff88147e44dcfdc1e376302
Nayan <nayan92001@gmail.com>
2024-08-04 21:09:58 +0530

    Implemented Expense Trends => displays a bar chart signifying category wise expenses

--- src/components/ExpenseTrends/ExpenseTrends.jsx
+++ src/components/ExpenseTrends/ExpenseTrends.jsx
@@ -1,11 +1,22 @@
+import { useContext } from "react";
+import CustomBar from "../Chart/Bar/CustomBar";
 import styles from "./ExpenseTrends.module.css";
+import CategoricalExpenseContext from "../Contexts/CategoricalExpenseContext";
+import { GoHistory } from "react-icons/go";
 
 const ExpenseTrends = () => {
+
+    const [categoricalDetailsObject] = useContext(CategoricalExpenseContext);
+    const { categoricalDetails } = categoricalDetailsObject;
+
     return <>
         <div className={styles.trends}>
             <h1>Expense Trends</h1>
             <section className={styles.chart}>
-
+                {!categoricalDetails || categoricalDetails.length == 0 ?
+                    <div className={styles.empty}>
+                        <h2 ><GoHistory /> No expenses accounted yet...</h2>
+                    </div> : <CustomBar />}
             </section>
         </div>
     </>
